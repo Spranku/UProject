e20877a5ffa87c19c1830422b30312443b8f7152
Spranky <fvs@imt.zone>
2026-03-21 16:39:48 +0300

    Calculator ready

---
+++ Assets/MyScripts/CalculationButton.cs
@@ -0,0 +1,155 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Actions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculationButton : MonoBehaviour
+{
+    enum EAction
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
+    Button inButton;
+
+    public TMP_InputField leftSide;
+    public TMP_InputField rightSide;
+    public TMP_Text Result;
+
+    public void OnClick(Button inButton)
+    {
+        /* Save current sign of button */
+        var buttonText = inButton.GetComponentInChildren<TMP_Text>();
+
+        /* Choice func by sign */
+        switch (buttonText.text)
+        {
+            case "+":
+            {
+                    TryToGiveResult(leftSide,rightSide,EAction.Addition);
+                    break;
+            }
+
+            case "-":
+            {
+                    TryToGiveResult(leftSide, rightSide, EAction.Substraction);
+                    break;
+            }
+
+            case "*":
+            {
+                    TryToGiveResult(leftSide, rightSide, EAction.Multiplication);
+                    break; 
+            }
+
+            case "/":
+            {
+                    TryToGiveResult(leftSide, rightSide, EAction.Division);
+                    break;
+            }
+
+            default:
+                { break; }
+        }
+    }
+
+    private void TryToGiveResult(TMP_InputField left, TMP_InputField right, EAction actionType)
+    {
+        if(!CheckInput(left, right)) return;
+
+        switch (actionType)
+        {
+            case EAction.Addition:
+                {
+                    Addition(left,right);
+                    break;
+                }
+
+            case EAction.Substraction:
+                {
+                    Substraction(left, right);
+                    break;
+                }
+
+            case EAction.Multiplication:
+                {
+                    Multiplication(left,right);
+                    break;
+                }
+
+            case EAction.Division:
+                {
+                    Division(left,right);
+                    break;
+                }
+        }
+    }
+
+    public bool CheckInput(TMP_InputField A, TMP_InputField B)
+    {
+        /* Check nullptr */
+        if (A == null || B == null) return false;
+
+        /*Check empty */
+        if (A.text == "" || B.text == "") return false;
+
+        /* Check convert to int */
+        if (!int.TryParse(A.text, out _) || !int.TryParse(B.text, out _)) return false;
+
+        return true;
+    }
+
+    private void Addition(TMP_InputField A, TMP_InputField B)
+    {
+        int result = 0;
+
+        if (int.TryParse(A.text, out int a) && int.TryParse(B.text, out int b))
+        {
+            result = a + b;
+        }
+
+        Result.text = result.ToString();
+    }
+
+    private void Substraction(TMP_InputField A, TMP_InputField B)
+    {
+        int result = 0;
+
+        if (int.TryParse(A.text, out int a) && int.TryParse(B.text, out int b))
+        {
+            result = a - b;
+        }
+
+        Result.text = result.ToString();
+    }
+
+    private void Multiplication(TMP_InputField A, TMP_InputField B)
+    {
+        int result = 0;
+
+        if (int.TryParse(A.text, out int a) && int.TryParse(B.text, out int b))
+        {
+            result = a * b;
+        }
+
+        Result.text = result.ToString();
+    }
+
+    private void Division(TMP_InputField A, TMP_InputField B)
+    {
+        int result = 0;
+
+        if (int.TryParse(A.text, out int a) && int.TryParse(B.text, out int b))
+        {
+            result = a / b;
+        }
+
+        Result.text = result.ToString();
+    }
+
+}
