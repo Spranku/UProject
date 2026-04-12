2dde67ed24b74f028d5529ec6a8211bad6e49c4a
Spranky <fvs@imt.zone>
2026-04-12 23:09:56 +0300

    Pinball setup

--- Assets/MyScripts/Module_10/LaunchPlatform.cs
+++ Assets/MyScripts/Module_10/LaunchPlatform.cs
@@ -7,18 +7,13 @@ public class LaunchPlatform : MonoBehaviour
     public float LaunchPeriod = 1.0f;
     public float LauchPower = 100.0f;
 
-    private Vector3 SourcePosition = Vector3.zero;
     private float CachePeriod;
-    ConfigurableJoint ConfJoint;
     Rigidbody rg;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         /* Save source period for clear to default after launch */
         CachePeriod = LaunchPeriod;
-        SourcePosition = transform.position;
-        ConfJoint = GetComponent<ConfigurableJoint>();
-
         rg = GetComponent<Rigidbody>();
     }
 
@@ -34,10 +29,7 @@ public class LaunchPlatform : MonoBehaviour
 
     private void Launch()
     {
-        Debug.Log("=======Launch=======");
-
         rg.AddForce(new Vector3(transform.position.x, transform.position.y - (-0.01f) * LauchPower, transform.position.z), ForceMode.Impulse);
-
         LaunchPeriod = CachePeriod;
     }
 }
