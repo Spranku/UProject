6cb3612460a3236580d0494e8cebe551769d8523
Spranky <fvs@imt.zone>
2026-04-26 19:37:15 +0300

    Last level added

--- Assets/MyScripts/Module_11/PlayerController.cs
+++ Assets/MyScripts/Module_11/PlayerController.cs
@@ -4,11 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool IsLastLevel = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("GameController"))
+        if(other.CompareTag("GameController") && !IsLastLevel)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if(other.CompareTag("GameController") && IsLastLevel)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 0);
         }
     }
 }
