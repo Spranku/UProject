b0d8750109e927bc84b0a2400520356ccfbb6b0b
Spranky <fvs@imt.zone>
2026-05-03 20:02:06 +0300

    Setup winner VFX

--- Assets/MyScripts/Module_11/PlayerController.cs
+++ Assets/MyScripts/Module_11/PlayerController.cs
@@ -1,10 +1,34 @@
-using Unity.VectorGraphics;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public bool IsLastLevel = false;
+    public Canvas WinnerCanvas = null;
+    public GameObject WinnerVFX = null;
+
+    private void Start()
+    {
+        WinnerCanvas.gameObject.SetActive(false);
+    }
+
+    public void OnShowWinnerScreen()
+    {
+        if(WinnerCanvas)
+        {
+            WinnerCanvas.gameObject.SetActive(true);
+            if(WinnerVFX)
+            {
+                var Particles = WinnerVFX.GetComponentsInChildren<ParticleSystem>();
+                for(byte i = 0; i < Particles.Length; ++i)
+                {
+                    Particles[i].Play();
+                }
+            }
+            Coroutine coroutine = StartCoroutine(ReturnToMainMenuTimer());
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,7 +38,13 @@ public class PlayerController : MonoBehaviour
         }
         else if(other.CompareTag("GameController") && IsLastLevel)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 0);
+            OnShowWinnerScreen();
         }
+    }
+
+    private IEnumerator ReturnToMainMenuTimer()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 0);
     }
 }
