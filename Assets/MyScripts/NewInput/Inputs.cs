b42dd528b7b97196419d3fa583f1baf6e28b3377
Spranky <fvs@imt.zone>
2026-07-15 13:06:43 +0300

    Interact change animations

--- Assets/MyScripts/NewInput/Inputs.cs
+++ Assets/MyScripts/NewInput/Inputs.cs
@@ -10,11 +10,15 @@ public class Inputs : MonoBehaviour
     public bool Num5;
 
     [SerializeField]
+    public TestHUD HUD;
+
+    [SerializeField]
     public Animator CharacterAnimator;
 
     public void OnNum1(InputValue value)
     {
         CharacterAnimator.SetBool("Num1", true);
+        if(HUD) { HUD.SetActiveElement(0); }
         Debug.Log("Num1");
         CharacterAnimator.SetBool("Num2", false);
         CharacterAnimator.SetBool("Num3", false);
@@ -25,6 +29,7 @@ public class Inputs : MonoBehaviour
     public void OnNum2(InputValue value)
     {
         CharacterAnimator.SetBool("Num2", true);
+        if (HUD) { HUD.SetActiveElement(1); }
         Debug.Log("Num2 ");
         CharacterAnimator.SetBool("Num1", false);
         CharacterAnimator.SetBool("Num3", false);
@@ -35,6 +40,7 @@ public class Inputs : MonoBehaviour
     public void OnNum3(InputValue value)
     {
         CharacterAnimator.SetBool("Num3", true);
+        if (HUD) { HUD.SetActiveElement(2); }
         Debug.Log("Num3");
         CharacterAnimator.SetBool("Num1", false);
         CharacterAnimator.SetBool("Num2", false);
@@ -45,6 +51,7 @@ public class Inputs : MonoBehaviour
     public void OnNum4(InputValue value)
     {
         CharacterAnimator.SetBool("Num4", true);
+        if (HUD) { HUD.SetActiveElement(3); }
         Debug.Log("Num4");
         CharacterAnimator.SetBool("Num1", false);
         CharacterAnimator.SetBool("Num2", false);
@@ -55,6 +62,7 @@ public class Inputs : MonoBehaviour
     public void OnNum5(InputValue value)
     {
         CharacterAnimator.SetBool("Num5", true);
+        if (HUD) { HUD.SetActiveElement(4); }
         Debug.Log("Num5");
         CharacterAnimator.SetBool("Num1", false);
         CharacterAnimator.SetBool("Num2", false);
