31d1b509fa5b4546dc02b8f713734e60431adfd5
Spranky <fvs@imt.zone>
2026-07-15 11:51:00 +0300

    Choice animations

--- Assets/MyScripts/NewInput/Inputs.cs
+++ Assets/MyScripts/NewInput/Inputs.cs
@@ -9,28 +9,56 @@ public class Inputs : MonoBehaviour
     public bool Num4;
     public bool Num5;
 
+    [SerializeField]
+    public Animator CharacterAnimator;
+
     public void OnNum1(InputValue value)
     {
+        CharacterAnimator.SetBool("Num1", true);
         Debug.Log("Num1");
+        CharacterAnimator.SetBool("Num2", false);
+        CharacterAnimator.SetBool("Num3", false);
+        CharacterAnimator.SetBool("Num4", false);
+        CharacterAnimator.SetBool("Num5", false);
     }
 
     public void OnNum2(InputValue value)
     {
+        CharacterAnimator.SetBool("Num2", true);
         Debug.Log("Num2 ");
+        CharacterAnimator.SetBool("Num1", false);
+        CharacterAnimator.SetBool("Num3", false);
+        CharacterAnimator.SetBool("Num4", false);
+        CharacterAnimator.SetBool("Num5", false);
     }
 
     public void OnNum3(InputValue value)
     {
+        CharacterAnimator.SetBool("Num3", true);
         Debug.Log("Num3");
+        CharacterAnimator.SetBool("Num1", false);
+        CharacterAnimator.SetBool("Num2", false);
+        CharacterAnimator.SetBool("Num4", false);
+        CharacterAnimator.SetBool("Num5", false);
     }
 
     public void OnNum4(InputValue value)
     {
+        CharacterAnimator.SetBool("Num4", true);
         Debug.Log("Num4");
+        CharacterAnimator.SetBool("Num1", false);
+        CharacterAnimator.SetBool("Num2", false);
+        CharacterAnimator.SetBool("Num3", false);
+        CharacterAnimator.SetBool("Num5", false);
     }
 
     public void OnNum5(InputValue value)
     {
+        CharacterAnimator.SetBool("Num5", true);
         Debug.Log("Num5");
+        CharacterAnimator.SetBool("Num1", false);
+        CharacterAnimator.SetBool("Num2", false);
+        CharacterAnimator.SetBool("Num3", false);
+        CharacterAnimator.SetBool("Num4", false);
     }
 }
